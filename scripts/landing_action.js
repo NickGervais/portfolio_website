a6848d00125884bc5e7f2db02dd05b6ab2a8abeb
NickGervais <nicholaspgervais@gmail.com>
2019-05-30 17:34:55 -0600

    added cta for about me section

--- scripts/landing_action.js
+++ scripts/landing_action.js
@@ -1,17 +1,22 @@
-$("#landing").on('click', function(){
-    console.log('hi');
-    console.log($("#landing").hasClass("landing-action"));
+let landing_action = function(){
     if($("#landing").hasClass("landing-action")){
         $("#landing").removeClass("landing-action");
         $("#title").removeClass("title-action");
         $("#my-name").removeClass("my-name-action");
         $("#me").removeClass("me-action");
         $("#about-me").removeClass("about-me-action");
+        $("#landing-cta span").text("more about me");
     } else {
         $("#landing").addClass("landing-action");
         $("#title").addClass("title-action");
         $("#my-name").addClass("my-name-action");
         $("#me").addClass("me-action");
         $("#about-me").addClass("about-me-action");
+        $("#landing-cta span").text("less about me")
     }
+
+};
+
+$("#landing-cta span").on('click', function(){
+    landing_action();
 });
